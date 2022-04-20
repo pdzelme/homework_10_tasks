ffd8815d58e9aaea4b741b472b2d75e724ddb3a8
Paula Paula <p@Paulas-MacBook-Air.local>
2022-04-20 17:16:05 +0300

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // TASK1
+        //Task1 task1 = new Task1();
+        //task1.calculate();
+
+        // TASK2
+        //Task2 task2 = new Task2();
+        //task2.calculate();
+
+        // TASK3
+        //Task3 task3 = new Task3();
+        //task3.calculate();
+
+        // TASK4
+        //Task4 task4 = new Task4();
+        //task4.calculate();
+
+        // TASK5
+        //Task5 task5 = new Task5();
+        //task5.calculate();
+
+        // TASK6
+        //Task6 task6 = new Task6();
+        //task6.calculate();
+
+        // TASK7
+        //Task7 task7 = new Task7();
+        //task7.calculate();
+
+        // TASK8
+        //Task8 task8 = new Task8();
+        //task8.calculate();
+
+        // TASK9
+        //Task9 task9 = new Task9();
+        //task9.calculate();
+
+        // TASK10
+        //Task10 task10 = new Task10();
+        //task10.calculate();
+    }
+}
